6db22fe8d242eb1309526373242b5b6dedb84aba
garikinasailu <garikinasailu6@gmail.com>
2025-01-29 20:10:46 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,119 @@
+#Syntax
+#if condition:
+#    statement 1
+#    statement 2
+#    statement n 
+
+#age=16
+#if age>=18:
+#    print("you are eligible  to vote ")
+#    print(f"your {age}   years older")
+#print("this is outside of block of code")
+
+#number=16
+#if number>14:
+#    print(number *number)
+#print("next line of the code")
+
+#else Syntax
+#if condition:
+#     block of code
+#    the block of code true then executed
+#else :
+#    alternative block of code
+#    the block of code false then executed
+
+
+#age=17
+#if age>=18:
+#    print(f"your eligible the vote {age}")
+#else:
+#    print(f" your not eligible{age}")
+#    print(f"your age is {age}")
+
+#user_name=input("enter the user name:")
+#user_password= int(input("enter the user password:"))
+#if user_name=="vasu" and user_password==123:
+#    print("login is successful")
+#else:
+#    print("invaild credential")
+
+#syntax of if elif else
+#if condition1:
+#    statement1
+#elif condition2:
+#    statement2
+#elif condition3:
+#     statement3
+#.......
+#else:
+#    statement
+
+#grade system checking.....
+#marks=int(input("enter the marks:"))
+#if marks>=90 :
+#    print(f"your got A grad obtained{marks} marks")
+#elif marks>=70:
+#    print(f"your got B you obtained {marks}marks") 
+#elif marks>=45:
+#    print(f"your got C you obtained {marks}marks")
+#elif marks>=35:
+#    print(f"your  pass {marks}marks")
+#else:
+#    print (f"your fail you got  {marks} marks")
+
+
+#syntax nested if else
+#if condition:
+#    code statement1
+#    if condition2:
+#        code statement2
+#    else:
+#        code statement
+#else:
+#    code of statement 
+
+#user_name=input("enter the  user_name:")
+#user_id=int(input("enter the user_id:"))
+#if user_name=="sailu":
+#    if user_id==123:
+#        print("login success")
+#    else:
+#       print("invaild user_id")
+#else:
+#    print("invaild user_name")
+#print("invaild user_name and user_id")
+
+#short hand if Syntax
+#result=value_if_true if condition else value_if_false
+
+#print ("5 is greater than 3") if 5>3 else print("5 is not greater than 3" )
+
+number=int(input ("enter the number:"))
+print(f" {number} even number") if number%2==0 else print(f" {number} odd number")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
